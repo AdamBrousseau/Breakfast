b5901f51a07ec2b0745d69d35b793a3656c81f84
Adam Brousseau <adam.brousseau88@gmail.com>
2014-02-18 21:01:37 -0500

    schema update with latest chanegs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212010537) do
+ActiveRecord::Schema.define(version: 20140212011318) do
 
   create_table "ailments", force: true do |t|
     t.string   "ailment_name"
@@ -73,6 +73,8 @@ ActiveRecord::Schema.define(version: 20140212010537) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "deleted",        default: false
+    t.string   "comment2"
+    t.string   "doctor"
   end
 
   add_index "eyes", ["phr_id", "date"], name: "index_eyes_on_phr_id_and_date"
