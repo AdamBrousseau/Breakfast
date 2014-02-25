25e042412c57cf419d27a890f1c5a8b60ada3038
forkbattery <forkbattery@gmail.com>
2014-02-25 01:44:46 -0500

    Got pagination working on phonebook

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,8 +4,8 @@ class ContactsController < ApplicationController
 
 	
 	def index
-		#@contacts = current_user.contacts.paginate(page: params[:page], per_page: 10)
-		@contacts = current_user.contacts.all
+		@contacts = current_user.contacts.paginate(page: params[:page], per_page: 5)
+		#@contacts = current_user.contacts.all
 	end
 
 	def show
