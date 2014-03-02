888c708c07baeae52522a50263dfc1bbe56e9cbe
forkbattery <forkbattery@gmail.com>
2014-03-02 03:23:50 -0500

    Added PDF generate feature

---
+++ app/pdfs/contact_pdf.rb
@@ -0,0 +1,36 @@
+class ContactPdf < Prawn::Document
+
+  def initialize(contacts)
+    super()
+
+    #Header
+    bounding_box [bounds.left, bounds.top], :width  => bounds.width do
+        font "Times-Roman"
+        text "Contacts", :align => :center, :size => 25
+        text "<br>", :inline_format => true
+        stroke_horizontal_rule
+        text "<br>", :inline_format => true
+    end  
+
+    @contacts=contacts
+    @contacts.each do |contact|
+    	font "Times-Roman", :size => 13
+    	text "Name\: #{contact.name}"
+    	text "Primary Phone\: #{contact.phone1}"
+    	text "Secondary Phone\: #{contact.phone2}"
+    	text "Email\: #{contact.email}"
+    	text "Description\: #{contact.description}"
+    	text "<br>", :inline_format => true   
+        stroke do
+			stroke_color '#d3d3d3'
+			dash(5, space: 2, phase: 0)
+			horizontal_line(0, 230)
+		end
+		text "<br>", :inline_format => true
+    end
+
+    #Footer
+    number_pages "<page>", { :start_count_at => 0, :page_filter => :all, :at => [bounds.right - 50, 0], :align => :right, :size => 12 }
+  end
+
+end
