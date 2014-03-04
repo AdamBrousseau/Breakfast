3b48365b5f6a3a08f5cfadedbcd62ebd99308eae
forkbattery <forkbattery@gmail.com>
2014-03-03 20:30:53 -0500

    Removed diagnosis for Blood Pressure

--- app/controllers/bps_controller.rb
+++ app/controllers/bps_controller.rb
@@ -3,19 +3,6 @@ class BpsController < ApplicationController
 		@phr = Phr.find(params[:phr_id])
 		@bps = @phr.bps.paginate(page: params[:page], per_page: 5)
 		#@bps = @phr.bps.paginate(page: params[:page], per_page: 5)
-		@bps.each do |bp|
-			if (bp.systolic >= 80 and bp.systolic <= 119) && (bp.diastolic >= 60 and bp.diastolic <= 79)
-				bp.condition = 'Normal'
-			elsif (bp.systolic >= 120 and bp.systolic <= 139) || (bp.diastolic >= 80 and bp.diastolic <= 89)
-				bp.condition = 'Pre-hypertension'
-			elsif (bp.systolic >= 140 and bp.systolic <= 159) || (bp.diastolic >= 90 and bp.diastolic <= 99)
-				bp.condition = 'Hypertension, Stage 1'
-			elsif (bp.systolic > 159) || (bp.diastolic > 99)
-				bp.condition = 'Hypertension, Stage 2'
-			else
-				bp.condition = 'Hypotension'
-			end
-		end
 	end
 
 	def new
