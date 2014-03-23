c47469444ba2d6db73cf3a6f361051b3f04cd6ae
AdamAxtmann <adamaxtmann@gmail.com>
2014-03-23 15:44:45 -0400

    added dropdown in appointments to select doctor from contacts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140320230506) do
+ActiveRecord::Schema.define(version: 20140321061146) do
 
   create_table "ailments", force: true do |t|
     t.string   "ailment_name"
@@ -208,7 +208,9 @@ ActiveRecord::Schema.define(version: 20140320230506) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin",           default: false
+    t.boolean  "admin",            default: false
+    t.boolean  "active",           default: false
+    t.string   "activation_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
