b8d0576a8594843af310b46b6bf1d79163d8e7ab
AdamAxtmann <adamaxtmann@gmail.com>
2014-03-31 14:48:51 -0400

    added honorific column to contacts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140331170535) do
+ActiveRecord::Schema.define(version: 20140331183037) do
 
   create_table "ailments", force: true do |t|
     t.string   "ailment_name"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20140331170535) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "deleted",     default: false
+    t.string   "honorific"
   end
 
   add_index "contacts", ["user_id"], name: "index_contacts_on_user_id"
