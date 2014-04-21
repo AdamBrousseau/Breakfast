371b0640c5662483bfeffc7aee4d6220362e909c
Adam Brousseau <adam.brousseau88@gmail.com>
2014-04-21 17:45:42 -0400

    Setting up for production push

--- app/models/dental.rb
+++ app/models/dental.rb
@@ -2,7 +2,6 @@ class Dental < ActiveRecord::Base
 	acts_as_paranoid
 	belongs_to :phr
 
-	validates :dentist, presence: true
 	validates :date, presence: true
 	validates :description, presence: true
 end
